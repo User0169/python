e5ad4f08ddf233704254bf2085f6cd93b5a7e4b6
User0169 <softnextar@gmail.com>
2025-10-23 17:48:01 +0700

    first commit

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+helloworld = "hello world"
+print("hello world")
+
+helloworld2 = ["helloworld2"]
+print(helloworld2[0])
+
+def helloworld3():
+    return print("helloworld3")
+print(helloworld3())
+
+helloworld4 = {"hello":"world"}
+print(helloworld4)
+
+helloworld5 = "helloworld5"
+if helloworld5 == "helloworld5":
+    print("hello world5")
+
+helloworld6 = "helloworld6"
+for i in helloworld6:
+    print(i)
